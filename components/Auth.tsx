3ec46dc831390dde6b0da4b8a99c544c70986074
Takumikimura141899 <kimura141899@gmail.com>
2021-12-05 18:12:19 +0900

    [add] auth_function

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
+import Cookie from 'universal-cookie';
 import { LockClosedIcon } from '@heroicons/react/solid';
 
+const cookie = new Cookie();
+
 export const Auth = () => {
+  // 関数の中からページ遷移する際に使用する
+  const router = useRouter();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogin, setIsLogin] = useState(true);
+
+  const login = async () => {
+    try {
+      await fetch(
+        `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
+        {
+          method: 'POST',
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+        .then((res) => {
+          if (res.status === 400) {
+            throw 'authentication failed';
+          } else if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          const options = { path: '/' };
+          cookie.set('access_token', data.access, options);
+        });
+      router.push('/main-page');
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const authUser = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (isLogin) {
+      login();
+    } else {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, {
+          method: 'POST',
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }).then((res) => {
+          if (res.status === 400) {
+            throw 'authentication faired';
+          }
+        });
+        login();
+      } catch (err) {
+        alert(err);
+      }
+    }
+  };
+
   return (
     <>
       <div className='max-w-md w-full space-y-8'>
@@ -16,7 +80,7 @@ export const Auth = () => {
             />
           </div>
           <h2 className='mt-6 text-center text-3xl font-extrabold text-white'>
-            Sign in to your account
+            {isLogin ? 'Login' : 'Sing up'}
           </h2>
           <p className='mt-2 text-center text-sm text-gray-600'>
             Or{' '}
@@ -28,21 +92,25 @@ export const Auth = () => {
             </a>
           </p>
         </div>
-        <form className='mt-8 space-y-6' action='#' method='POST'>
+        <form className='mt-8 space-y-6' onSubmit={authUser}>
           <input type='hidden' name='remember' defaultValue='true' />
           <div className='rounded-md shadow-sm -space-y-px'>
             <div>
-              <label htmlFor='email-address' className='sr-only'>
-                Email address
+              <label htmlFor='username' className='sr-only'>
+                Username
               </label>
               <input
-                id='email-address'
-                name='email'
-                type='email'
-                autoComplete='email'
+                id='username'
+                name='username'
+                type='text'
+                autoComplete='username'
                 required
                 className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-                placeholder='Email address'
+                placeholder='Username'
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
               />
             </div>
             <div>
@@ -57,13 +125,18 @@ export const Auth = () => {
                 required
                 className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
                 placeholder='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </div>
 
           <div className='flex items-center justify-center'>
             <div className='text-sm'>
-              <span className='cursor-pointer font-medium text-white hover:text-indigo-500'>
+              <span
+                onClick={() => setIsLogin(!isLogin)}
+                className='cursor-pointer font-medium text-white hover:text-indigo-500'
+              >
                 Change mode ?
               </span>
             </div>
@@ -80,7 +153,7 @@ export const Auth = () => {
                   aria-hidden='true'
                 />
               </span>
-              Sign in
+              {isLogin ? 'Login with JWT' : 'Create new user'}
             </button>
           </div>
         </form>
