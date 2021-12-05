9536e523a7c48e681e760e4f9770624da43a6367
Takumikimura141899 <kimura141899@gmail.com>
2021-12-05 20:56:31 +0900

    [add] mainpage registercheck_ok

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -9,6 +9,7 @@ const cookie = new Cookie();
 export const Auth = () => {
   // 関数の中からページ遷移する際に使用する
   const router = useRouter();
+  console.log(process.env.NEXT_PUBLIC_RESTAPI_URL);
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -126,7 +127,9 @@ export const Auth = () => {
                 className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
                 placeholder='Password'
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
               />
             </div>
           </div>
